15c81a06aaa21b46a190025771513f7b9d481d34
Willi Kappler <willi-kappler@users.noreply.github.com>
2015-12-28 01:01:17 +0100

    fixed small typo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Mandelbrot set in rust
 //
-// This code shows how to calculate the set in serial and parallel_mandel
+// This code shows how to calculate the set in serial and parallel
 // More parallel versions will be added in the future
 //
 // Written by Willi Kappler
