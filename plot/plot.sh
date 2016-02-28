aa8bca33b2b4da08b474aa69d5e3d60c28037433
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-02-28 22:43:13 +0100

    update benchmark result and plot

--- plot/plot.sh
+++ plot/plot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gnuplot <<PLOT
-    set terminal png size 640,480
+    set terminal png size 800,600
     set output "mandel_bench.png"
     set view map
     set xlabel "number of cores"
@@ -9,8 +9,12 @@ gnuplot <<PLOT
     set style data points
     set title "mandelbrot benchmark"
     set xtics 0,2,24
+    set mxtics 2
     set xrange [0:25]
+
     set yrange [0:*]
+    set ytics 0,1000
+    set mytics 2
 
     filenames = "job_steal_join job_steal kirk_crossbeam rayon_join rayon_par_iter rust_scoped_pool scoped_thread_pool serial simple_parallel"
     plot for [file in filenames] file.".txt" title file with errorbars
