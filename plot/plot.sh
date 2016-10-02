65c1ed12e28e3ffb3ad7db2a8c1f1b49a3704f56
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-10-02 11:10:35 +0200

    fix plot script and image

--- plot/plot.sh
+++ plot/plot.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 gnuplot <<PLOT
-    set terminal png size 800,600
+    set terminal png noenhanced size 800,600
     set output "mandel_bench.png"
     set view map
     set xlabel "number of cores"
     set ylabel "time [ms]"
     set style data points
     set title "mandelbrot benchmark"
-    set xtics 10,1,24
+    set xtics 0,2,24
     set mxtics 2
-    set xrange [10:25]
+    set xrange [0:25]
 
     set yrange [500:*]
-    set ytics 0,100
+    set ytics 0,500
     set mytics 2
 
     filenames = "job_steal_join job_steal rayon_join rayon_par_iter rust_scoped_pool scoped_thread_pool serial"
-    plot for [file in filenames] file.".txt" using 1:(\$1>10?\$2:1/0):3:4 title file with errorbars
+    plot for [file in filenames] file.".txt" using 1:(\$1>0?\$2:1/0):3:4 title file with errorbars
 PLOT
